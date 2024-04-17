042e2f27be9d42ab79f8146bedbdd44cc8f9d772
Andrii-Cherevatov <Andrii.Cherevatov@fivesysdev.com>
2024-04-17 17:46:21 +0300

    Update chatConsts.js

--- src/core/chatConsts.js
+++ src/core/chatConsts.js
@@ -1,2 +1,2 @@
 export const tokenEndpointURL = 'https://directline.botframework.com/v3/directline/tokens/generate';
-export const dl_secret = "f7WqBQsIg_0.RkD3v_5cT4mmoHsPfHKElq0HHWyp3q5wVKu5oj0Pjto";
+export const dl_secret = "";
