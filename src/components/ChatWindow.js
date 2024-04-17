fdea80fb2b8ccc7eee3c2d261d42ad3c559f59dd
Andrii-Cherevatov <Andrii.Cherevatov@fivesysdev.com>
2024-04-17 14:38:49 +0300

    Minor enhancements

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,18 +1,18 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import ChatHeader from './ChatHeader.js';
 import './Styles/kahramaa.css';
 import { v4 as uuidv4 } from 'uuid';
 import styleOptions from './Styles/ChatStyles.js'
+import { tokenEndpointURL, dl_secret } from '../core/chatConsts.js';
 
 const ChatWindow = ({ onClose }) => {
     const webChatContainerRef = useRef(null);
     const [locale] = useState('en');
-    const tokenEndpointURL = 'https://directline.botframework.com/v3/directline/tokens/generate'; // move to consts
-    const dl_secret = "f7WqBQsIg_0.RkD3v_5cT4mmoHsPfHKElq0HHWyp3q5wVKu5oj0Pjto";
     
     useEffect(() => {
         async function fetchTokenAndRenderChat() { // move to action
             try {
+
                 let token = getCookie('chatToken');
                 let userId = getCookie('userId');
                 console.log(token);
@@ -65,7 +65,6 @@ const ChatWindow = ({ onClose }) => {
                 console.error('Failed to fetch token and render chat', error);
             }
         }
-        
         fetchTokenAndRenderChat();
     }, [locale]);
 
