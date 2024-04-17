3e029a6f35c381df6f9d03cd849fb42af4daf277
Andrii-Cherevatov <Andrii.Cherevatov@fivesysdev.com>
2024-04-17 17:21:57 +0300

    Apply structure changes

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,87 +1,15 @@
-import React, { useState, useRef, useEffect, useMemo } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ChatHeader from './ChatHeader.js';
 import './Styles/kahramaa.css';
-import { v4 as uuidv4 } from 'uuid';
-import styleOptions from './Styles/ChatStyles.js'
-import { tokenEndpointURL, dl_secret } from '../core/chatConsts.js';
+import { fetchTokenAndRenderChat } from '../store/actions.js';
 
 const ChatWindow = ({ onClose }) => {
     const webChatContainerRef = useRef(null);
     const [locale] = useState('en');
     
     useEffect(() => {
-        async function fetchTokenAndRenderChat() { // move to action
-            try {
-
-                let token = getCookie('chatToken');
-                let userId = getCookie('userId');
-                console.log(token);
-                if (!token) {
-                    // Token does not exist in cookies or has expired, fetch a new one
-                    userId = `dl_${uuidv4()}`; // Generate userId in the format "dl_[uuid]"
-                    console.log("We are here");
-                    const tokenResponse = await fetch(tokenEndpointURL, {
-                        method: 'POST',
-                        headers: {
-                            'Authorization': `Bearer ${dl_secret}`,
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            User: {
-                                id: userId,
-                                name: "User"
-                            }
-                        })
-                    }).then(response => response.json());
-                    
-                    console.log(tokenResponse);
-                    token = tokenResponse.token;
-                    
-                    let expirationTime = Date.now() + (tokenResponse.expires_in * 1000); // Convert expires_in to milliseconds
-                    setCookie('chatToken', token, expirationTime);
-                    setCookie('userId', userId); // Store userId in the cookie
-                }
-
-                const directLine = window.WebChat.createDirectLine({
-                    token: token
-                });
-
-                window.WebChat.renderWebChat({
-                    directLine,
-                    styleOptions,
-                    locale,
-                }, webChatContainerRef.current);
-
-                directLine.postActivity({
-                    from: { id: userId, name: 'User' },
-                    name: 'startConversation',
-                    type: 'event',
-                    value: ''
-                }).subscribe(
-                    id => console.log(`Posted activity, assigned ID ${id}`),
-                    error => console.log(`Error posting activity: ${error}`)
-                );
-            } catch (error) {
-                console.error('Failed to fetch token and render chat', error);
-            }
-        }
-        fetchTokenAndRenderChat();
-    }, [locale]);
-
-    function setCookie(name, value, expirationTime) {
-        document.cookie = `${name}=${value}; expires=${new Date(expirationTime).toUTCString()}; path=/; Secure; SameSite=Strict`;
-    }
-
-    function getCookie(name) {
-        const cookies = document.cookie.split(';');
-        for (let cookie of cookies) {
-            const [cookieName, cookieValue] = cookie.trim().split('=');
-            if (cookieName === name) {
-                return cookieValue;
-            }
-        }
-        return null;
-    }
+        fetchTokenAndRenderChat(locale, webChatContainerRef); // Call the action function
+      }, [locale]);
 
     function scrollToBottom() {
         const chatContainer = webChatContainerRef.current;
--- src/components/Chatcontainer.js
+++ src/components/Chatcontainer.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ChatWindow from './ChatWindow';
 
 const ChatContainer = () => {
   const [isChatOpen, setChatOpen] = useState(false);
