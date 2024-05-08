1c34e73d58e977dff4f946608612ebd2abc8291e
Andrii-Cherevatov <Andrii.Cherevatov@fivesysdev.com>
2024-05-08 18:04:32 +0300

    Upd secret setting

--- src/core/chatConsts.js
+++ src/core/chatConsts.js
@@ -1,2 +1,2 @@
 export const tokenEndpointURL = 'https://directline.botframework.com/v3/directline/tokens/generate';
-export const dl_secret = `${process.env.dl_secret}`;
+export const dl_secret = `${process.env.DL_SECRET}`;
